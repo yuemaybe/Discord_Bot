26d49b51f6f3b317aa0bb834f31bfb9bf59b0c9f
yuemaybe <58838655+yuemaybe@users.noreply.github.com>
2019-12-15 14:14:10 +0800

    Update discordtest.py

--- discordtest.py
+++ discordtest.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[4]:
-
-
 import discord
 from discord.ext import commands
 
@@ -12,11 +6,23 @@ bot = commands.Bot(command_prefix='!')
 @bot.event
 async def on_ready():
     print('機器人測試上線')
-    
+
+@bot.event
+async def on_member_join(member):
+    print(F'{member} 加入群組')
+    channel = bot.get_channel(654717730800533531)
+    await channel.send(F'{member} 加入群組！')
+
+@bot.event
+async def on_member_remove(member):
+    print(F'{member} 離開群組')
+    channel = bot.get_channel(654717730800533531)
+    await channel.send(F'{member} 離開群組！')
+
 bot.run('NjU0NzE4Nzg3MDI2NjgxODY2.XfJtog.MJCPfCkTFJNoWFQTy5BPHbaB2as')
 
 
-# In[ ]:
+
 
 
 
