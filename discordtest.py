f515c303aefaef9f2ca8749e7fdf207ef43938be
yuemaybe <58838655+yuemaybe@users.noreply.github.com>
2019-12-15 15:09:49 +0800

    Update discordtest.py

--- discordtest.py
+++ discordtest.py
@@ -1,25 +1,39 @@
 import discord
 from discord.ext import commands
+import json
+
+with open('setting.json', 'r', encoding='utf8') as jFile:
+    jdata = json.load(jFile)
+
 
 bot = commands.Bot(command_prefix='!')
 
 @bot.event
 async def on_ready():
     print('機器人測試上線')
+    channel = bot.get_channel(int(jdata['test_channel']))
+    await channel.send('機器人已上線  :100:')
 
 @bot.event
 async def on_member_join(member):
     print(F'{member} 加入群組')
-    channel = bot.get_channel(654717730800533531)
+    channel = bot.get_channel(int(jdata['test_channel']))
     await channel.send(F'{member} 加入群組！')
 
 @bot.event
 async def on_member_remove(member):
     print(F'{member} 離開群組')
-    channel = bot.get_channel(654717730800533531)
+    channel = bot.get_channel(int(jdata['test_channel']))
     await channel.send(F'{member} 離開群組！')
 
-bot.run('NjU0NzE4Nzg3MDI2NjgxODY2.XfJtog.MJCPfCkTFJNoWFQTy5BPHbaB2as')
+@bot.command()
+async def ping(ctx):
+    await ctx.send(F'{round(bot.latency * 1000)} ms')
+
+
+
+
+bot.run(jdata['TOKEN'])
 
 
 
